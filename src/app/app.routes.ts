fb76ec5099da9c198a547eda830e4a7f15977c3f
Jay Patel <jay.a.patel@simformsolutions.com>
2025-05-01 14:36:15 +0530

    feat: setup login route in the app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { LoginComponent } from './pages/auth/login/login.component';
+
+export const routes: Routes = [
+  // Default Route redirected to the login page
+  {
+    path: '',
+    redirectTo: '/auth/login',
+    pathMatch: 'full',
+  },
+  // login page
+  {
+    path: 'auth/login',
+    component: LoginComponent,
+  },
+];
