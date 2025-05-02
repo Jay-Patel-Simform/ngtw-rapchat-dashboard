1edb690768a1ea44b3177303b7379bc56ec69573
Jay Patel <jay.a.patel@simformsolutions.com>
2025-05-02 11:34:04 +0530

    refactor: Button Component with CSS Variables

--- src/app/components/atoms/button/button.component.ts
+++ src/app/components/atoms/button/button.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -8,17 +8,17 @@ import { Component, Input, input } from '@angular/core';
 })
 export class ButtonComponent {
   // button props
-  variant = input<'primary' | 'secondary'>('primary');
+  @Input() variant: 'primary' | 'secondary' = 'primary';
   @Input() buttonType: HTMLButtonElement['type'] = 'button';
 
   get buttonClasses() {
     const baseClasses =
-      'h-[48px] rounded-[8px] text-center text-sm font-semibold cursor-pointer text-white transition duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white disabled:cursor-not-allowed disabled:opacity-50 w-full shadow-[0_4px_12px_0_#3751FF3D] tracking-[0.2px]';
+      'h-12 rounded-lg text-center text-sm font-semibold cursor-pointer text-white transition duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white disabled:cursor-not-allowed disabled:opacity-50 w-full shadow-primary tracking-[0.2px]';
 
     const variantClasses = {
-      primary: 'bg-[rgba(55,81,255,1)]',
+      primary: 'bg-primary hover:bg-primary/80 focus:ring-primary',
       secondary: 'bg-gray-500',
-    }[this.variant()];
+    }[this.variant];
 
     return `${baseClasses} ${variantClasses}`;
   }
