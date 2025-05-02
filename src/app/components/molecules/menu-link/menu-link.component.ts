e34d91f0a344c33b61ed0c05d4b135cf751e7ea1
Jay Patel <jay.a.patel@simformsolutions.com>
2025-05-02 14:01:14 +0530

    refactor: Apply Tailwind theme colors into molecules folder

--- src/app/components/molecules/menu-link/menu-link.component.ts
+++ src/app/components/molecules/menu-link/menu-link.component.ts
@@ -23,15 +23,20 @@ import { AngularSvgIconModule } from 'angular-svg-icon';
   `,
 })
 export class MenuLinkComponent {
-  isActive = false;
+  // Props
   @Input() href = '#';
   @Input() label = '';
   @Input() icon = '';
+  // State variable to track if the link is active
+  isActive = false;
 
+  // Subscription to track router events
   private routeSubscription!: Subscription;
 
+  // Inject the Angular Router Service
   constructor(private readonly router: Router) {}
 
+  // Runs once after the component is initialized
   ngOnInit() {
     this.routeSubscription = this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
@@ -39,11 +44,14 @@ export class MenuLinkComponent {
       }
     });
 
+    // Initial check to set the active state
     this.isActive = this.router.url.startsWith(this.href);
   }
 
+  // Runs once  before the component is destroyed
   ngOnDestroy() {
     if (this.routeSubscription) {
+      // Unsubscribe from events to prevents memory leaks
       this.routeSubscription.unsubscribe();
     }
   }
