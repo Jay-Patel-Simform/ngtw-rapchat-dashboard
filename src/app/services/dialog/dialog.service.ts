7f86147e1033427f514ccbd69391bcebf6ff5b45
Jay Patel <jay.a.patel@simformsolutions.com>
2025-05-08 18:53:46 +0530

    partial commit : added code that not fully working trying to load modal outside app-root like portal

--- src/app/services/dialog/dialog.service.ts
+++ src/app/services/dialog/dialog.service.ts
@@ -1,4 +1,12 @@
-import { Injectable, signal } from '@angular/core';
+import {
+  ApplicationRef,
+  ComponentRef,
+  createComponent,
+  EnvironmentInjector,
+  Injectable,
+  signal,
+} from '@angular/core';
+import { DialogComponent } from '../../components/dialog/dialog.component';
 
 @Injectable({
   providedIn: 'root',
@@ -6,11 +14,38 @@ import { Injectable, signal } from '@angular/core';
 export class DialogService {
   dialogStates = signal<Record<string, boolean>>({});
 
+  private dialogRef?: ComponentRef<DialogComponent>;
+
+  constructor(
+    private readonly appRef: ApplicationRef,
+    private readonly envInjector: EnvironmentInjector
+  ) {}
+
   open(dialogId: string) {
+    if (this.dialogRef) return;
+
+    // Create the component with Environment
+    this.dialogRef = createComponent(DialogComponent, {
+      environmentInjector: this.envInjector,
+    });
+
+    // Attach to the application view tree
+    this.appRef.attachView(this.dialogRef.hostView);
+
+    // Append DOM Element to the body
+    const domElement = this.dialogRef.location.nativeElement;
+    document.body.appendChild(domElement);
+
     this.dialogStates.update(val => ({ ...val, [dialogId]: true }));
   }
 
   close(dialogId: string) {
+    if (this.dialogRef) {
+      this.appRef.detachView(this.dialogRef.hostView);
+      this.dialogRef?.destroy();
+      this.dialogRef = undefined;
+    }
+
     this.dialogStates.update(val => ({ ...val, [dialogId]: false }));
   }
 
